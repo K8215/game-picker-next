f9bf476f85d546ca9209ff839d9a083fc75c5a4d
Kaitlyn Coleman <k8lak8@protonmail.com>
2023-08-29 18:25:30 -0400

    commented out basepath and static parameters for build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-	basePath: "/projects/game-picker-next",
+	//output: "export",
+	//basePath: "/projects/game-picker-next",
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
